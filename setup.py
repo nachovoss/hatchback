f277bf05d227af806411dddf9c0bef8040a94f46
Nacho <ignaciobaresmosca@gmail.com>
2025-12-29 17:18:59 +0100

    feat: Bump version to 0.1.5 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="hatchback",
-    version="0.1.0",
+    version="0.1.5",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
