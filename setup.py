65846e286dc2a85cf8b7a90da9c922fe2cdad476
Nacho <ignaciobaresmosca@gmail.com>
2025-12-29 17:15:07 +0100

    fix: Revert version number in setup.py to 0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="hatchback",
-    version="0.1.4",
+    version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
